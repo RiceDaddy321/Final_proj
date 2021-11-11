abca38b3bcb8ef95a8baa81c9091a47b936135f9
RiceDaddy321 <0carlitos2@gmail.com>
2021-11-11 12:52:04 -0600

    Completed the palette and general look of the website. Not it's about figuring out how to make the buttons work.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-});
+const app = express();
+
+//pull in axios
+const axios = require('axios');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+//for debugging
+var counter = 0;
+var btn = document.querySelector(".button");
+
+function updateCounter()
+{
+    counter++;
+    console.log(counter);
+}
+
+btn.addEventListener("click", updateCounter);
