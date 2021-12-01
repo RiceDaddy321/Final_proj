830092845e5bd7c62c663679468a7d348957d3a3
RiceDaddy321 <0carlitos2@gmail.com>
2021-12-01 11:47:00 -0600

    refactored code, and it now actually displays the information from the api for the random option.

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-//create express instance
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const htmlParser = require('htmlparser');
-const path = require('path');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//start express
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
-//important stuff
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/public'));
-app.use(express.static(path.join(__dirname + '/public')));
 
-//make the axios variable
-const axios = require("axios");
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//try to use external module
-var birds = require('./public/scripts/bird');
-var home = require('./public/scripts/buttons');
-app.use('/birds', birds);
-app.use('/', home);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.listen(3000, () => console.log("server has been started"));
-  
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
